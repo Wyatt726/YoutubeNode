afe4fb217ac1f4d45559005cc3979b7eeac98aab
Daleyar <ali.daley72@gmail.com>
2021-12-03 15:21:10 -0500

    Successfully reformatted and tested POST for comments

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,9 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const Joi = require('joi');
+
+const replySchema = new mongoose.Schema({
+    replyBody: {type: String, required: true}
+})
 
 const commentSchema = new mongoose.Schema({
     videoID: {type: String, required: true},
@@ -9,15 +14,25 @@ const commentSchema = new mongoose.Schema({
 
 })
 
-const replySchema = new mongoose.Schema({
-    replyBody: {type: String, required: true}
-})
 
 const Comment = mongoose.model('Comment', commentSchema)
 const Reply = mongoose.model('Reply', replySchema)
 
+function validateComment(comment) {
+    const schema = Joi.object ({
+        videoID: Joi.string().min(4).max(100).required(),
+        commentBody: Joi.string().min(2).max(100).required(),
+        likes: Joi.number(),
+        dislikes: Joi.number(),
+        replies: Joi.string().min(2).max(100)    
+    })
+    return schema.validate(comment)
+}
+
 module.exports = {
     Comment: Comment,
-    Reply: Reply
+    Reply: Reply,
+    commentSchema: commentSchema,
+    validate: validateComment
 }
 
--- routes/comments.js
+++ routes/comments.js
@@ -1,23 +1,26 @@
-const Comment = require('../models/comment');
+const {Comment, validate} = require('../models/comment');
 const express = require('express');
 const router = express.Router();
 
 //All endpoints and route handlers go here
-router.post('/',async(req,res)=>{
+router.post('/', async (req,res) =>{
     try{
-        const Comment = newComment({
-            VideoID: {type: String, required: true},
-            commentBody: {type: String, required: true},
-            likes: {type: Number, default: 0},
-            dislikes: {type: Number, default: 0},
-            replies: [replySchema]
-        });
-        awaitproduct.save();
+        const { error } = validate(req.body);
+        if( error )
+            return res.status(400).send(error);
 
-        returnres.send(product);
+        const comment = new Comment({
+            videoID: req.body.videoID,
+            commentBody: req.body.commentBody,
+            likes: req.body.likes,
+            dislikes: req.body.dislikes,
+        });
+        await comment.save();
+
+        return res.send(comment);
 
     }catch(ex){
-        returnres.status(500).send(`InternalServerError:${ex}`);
+        return res.status(500).send(`InternalServerError:${ex}`);
     }});
 
 module.exports = router;
