445149db616d7c7c0083a657fe11ffe9df5ae1b4
Wyatt726 <wyatt726@aol.com>
2021-12-03 14:26:32 -0500

    setting up endpoints for cmments

--- routes/comments.js
+++ routes/comments.js
@@ -3,5 +3,21 @@ const express = require('express');
 const router = express.Router();
 
 //All endpoints and route handlers go here
+router.post('/',async(req,res)=>{
+    try{
+        const Comment = newComment({
+            VideoID: {type: String, required: true},
+            commentBody: {type: String, required: true},
+            likes: {type: Number, default: 0},
+            dislikes: {type: Number, default: 0},
+            replies: [replySchema]
+        });
+        awaitproduct.save();
+
+        returnres.send(product);
+
+    }catch(ex){
+        returnres.status(500).send(`InternalServerError:${ex}`);
+    }});
 
 module.exports = router;
