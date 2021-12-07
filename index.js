6159afab74c7ffc51a8a12047508cc3ba1ed2ec1
Daleyar <ali.daley72@gmail.com>
2021-12-07 14:52:42 -0500

    Added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const connectDB = require('./startup/db');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const comments = require('./routes/comments');
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/comments', comments);
 
